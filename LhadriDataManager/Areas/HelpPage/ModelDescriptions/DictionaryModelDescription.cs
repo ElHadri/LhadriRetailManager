7bf9ba46841eecabbb312b1d569054b546c101fe
lhadri <el.hadri.b.adil@gmail.com>
2021-05-28 00:31:58 +0200

    Add the Web API Project
    Adds a blank Web API project and tweaks it.

---
+++ LhadriDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LhadriDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LhadriDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LhadriDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
