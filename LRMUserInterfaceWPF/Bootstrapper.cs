7341e968db6599ba68b0ffb549a320f5c4857d80
lhadri <el.hadri.b.adil@gmail.com>
2021-05-29 17:58:12 +0200

    Added: Caliburn.Micro framework

---
+++ LRMUserInterfaceWPF/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+
+using LRMUserInterfaceWPF.ViewModels;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LRMUserInterfaceWPF
+{
+    // to setup Caliburn.Micro
+    public class Bootstrapper : BootstrapperBase
+    {
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+    }
+}
