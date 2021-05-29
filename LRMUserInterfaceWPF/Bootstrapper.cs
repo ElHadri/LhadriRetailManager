5ca39004733570d5500981936e9073f610de4e86
lhadri <el.hadri.b.adil@gmail.com>
2021-05-29 21:16:06 +0200

    Added:  SimpleContainer DI container (Caliburn.Micro)

--- LRMUserInterfaceWPF/Bootstrapper.cs
+++ LRMUserInterfaceWPF/Bootstrapper.cs
@@ -14,10 +14,26 @@ namespace LRMUserInterfaceWPF
     // to setup Caliburn.Micro
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
 
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
@@ -25,5 +41,19 @@ namespace LRMUserInterfaceWPF
             DisplayRootViewFor<ShellViewModel>();
         }
 
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
     }
 }
